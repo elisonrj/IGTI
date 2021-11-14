e5aa1e0a0230a78fe13e1ed7b2d1574f34564bcf
Elison Pessôa <34030988+elisonrj@users.noreply.github.com>
2021-11-14 16:33:23 -0300

    add

--- EDC/infrastructure/s3_files.tf
+++ EDC/infrastructure/s3_files.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "job_spark" {
-  bucket = aws_s3_bucket.dl
+  bucket = "aws_s3_bucket.dl"
   key    = "emr-code/pyspark/job_spark_from_tf.py"
   acl   = "private"
   source = "../data/jobs/job_emr_spark.py"
